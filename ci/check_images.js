74dfa8690109ceac9ae571658ba49cbc1359e6bb
Wilhelm <va3093@gmail.com>
2022-01-28 15:14:23 +0000

    add docs

--- ci/check_images.js
+++ ci/check_images.js
@@ -8,6 +8,7 @@ const projectDirectories = fs.readdirSync(BASE_DIR)
 projectDirectories.forEach((projDir) => {
   let fileType = ""
   const imageFileBase = `${BASE_DIR}/${projDir}/${projDir}`
+  // Check that we have an image and what file type it is
   if (fs.existsSync(`${imageFileBase}.png`)) {
     fileType = 'png'
   } else if (fs.existsSync(`${imageFileBase}.svg`)) {
@@ -19,6 +20,9 @@ projectDirectories.forEach((projDir) => {
   const imageFile = `${imageFileBase}.${fileType}`
 
   const stats = fs.statSync(imageFile)
+  // In order to download images greater than 1Mb you need to use the github data api.
+  // Since these are just logos we should keep the build process simple and restrict the image
+  // sizes to less than 1 MB
   if (stats.size > 1048576) {
     throw new Error(`Image (${imageFile}) must be less than 1Mb. Instead it is ${stats.size} Bytes`)
   }
